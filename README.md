743c674000fbf4fc476902c3369c88234a982667
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2024-05-24 20:26:40 +0500

    feat(set_output): add

--- README.md
+++ README.md
@@ -11,4 +11,5 @@ Producing helpers for Ardour
 
 **Sends management**
 
-- **Add AUX Sends** Add aux sends to last selected route for all selected routes except the last one
+- **Add AUX Sends**. Add aux sends to last selected route for all selected routes except the last one
+- **Set output**. Set output of selected routes to last selected route
---
+++ set_outpuit.lua
@@ -0,0 +1,64 @@
+ardour {
+    ["type"] = "EditorAction",
+    name = "Set output",
+    author = "doojonio",
+    license = "GPL",
+    description = [[Set output of selected routes to last selected route]],
+}
+
+function factory()
+    local function connect_sides(output, input)
+        i = 0
+        while true do
+            out = output:audio(i)
+            inp = input:audio(i)
+            i = i + 1
+
+            if (out:isnil() or inp:isnil()) then
+                break
+            end
+
+            out:connect(inp:name())
+        end
+
+        i = 0
+        while true do
+            out = output:midi(i)
+            inp = input:midi(i)
+            i = i + 1
+
+            if (out:isnil() or inp:isnil()) then
+                break
+            end
+
+            out:connect(inp:name())
+        end
+    end
+
+    return function()
+        local sel = Editor:get_selection()
+
+        routes = sel.tracks:routelist()
+        if (routes:size() < 2) then
+            return
+        end
+
+        send_to = routes:back()
+        -- not bus protection
+        if (not send_to:to_track():isnil()) then
+            return
+        end
+
+        input = send_to:input()
+        for r in routes:iter() do
+            if (r == send_to) then
+                break
+            end
+
+            output = r:output()
+            output:disconnect_all(nil)
+
+            connect_sides(output, input)
+        end
+    end
+end
